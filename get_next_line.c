d8a761d24356c78eb6ad5afc2ed4cf7d2925719e
syberpunkq <syberpunkq@gmail.com>
2019-11-27 10:59:44 +0300

    it works

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,45 @@
 #include "get_next_line.h"
+
 int get_next_line(const int fd, char **line)
 {
-	return (read(fd, buf, BUF_SIZE));
+	int res;
+	int last;
+	int line_end;
+	char *buff;
+	char *ptr;
+	static char *temp;
+
+	line_end = 0;
+
+	*line = malloc(sizeof(char) * 1);
+	buff = malloc(sizeof(char) * BUF_SIZE + 1);
+	if (temp)
+	{
+		// last = ft_strlen(temp);
+		if((ptr = ft_strchr(temp, '\n')))
+		{
+			*ptr = 0;
+			*line = ft_strjoin(*line, temp);
+			temp = ptr + 1;
+			line_end = 1;
+		}
+		else
+		{
+			*line = ft_strjoin(*line, temp);
+		}
+	}
+	while (!line_end)
+	{
+		if ((res = read(fd, buff, BUF_SIZE)) == 0)
+			break;
+		buff[res] = 0;
+		if((ptr = ft_strchr(buff, '\n')))
+			{
+				*ptr = 0;
+				temp = ptr + 1;
+				line_end = 1;
+			}
+		*line = ft_strjoin(*line, buff);
+	}
+	return(1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <unistd.h>
-# define BUF_SIZE 32
+# include "../libft/libft.h"
+# define BUF_SIZE 10000
 #endif
 
